d3f90557c7d9f0150acef2c7f716719866c69806
Matthew <towski@gmail.com>
2010-06-02 22:29:39 -0700

    add a yml file for facebook

--- config.rb
+++ config.rb
@@ -6,8 +6,9 @@ require 'json'
 require 'curb'
 require 'history'
 
-API_KEY = "7fe35615dfcad9281348a8418a99fa58"
-SECRET_KEY = "30935b5f6fd56e5a99727cb812015f6b"
+facebook = YAML.load(File.open("config/facebook.yml").read)
+API_KEY = facebook["api_key"]
+SECRET_KEY = facebook["secret_key"]
 
 require 'memcache'
 CACHE = MemCache.new 'localhost:11211', :namespace => 'my_namespace'
