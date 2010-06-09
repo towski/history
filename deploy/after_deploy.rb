72fc64c4170bf1bb2c36fea8ae548541fdc1ad00
Matthew <towski@gmail.com>
2010-06-08 20:01:57 -0700

    before_migrate is the new

--- deploy/after_deploy.rb
+++
@@ -1,1 +0,0 @@
-run "ln -s /data/history/shared/config/facebook.yml /data/history/current/config/facebook.yml"
---
+++ deploy/before_migrate.rb
@@ -0,0 +1,1 @@
+run "ln -nfs #{shared_path}/config/facebook.yml #{release_path}/config/facebook.yml"
