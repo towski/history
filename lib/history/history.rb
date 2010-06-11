43ec677624801deccf4b3573504f1ef93c382a9e
Matthew <towski@gmail.com>
2010-06-11 15:38:35 -0700

    make fetch class level

--- lib/history/history.rb
+++ lib/history/history.rb
@@ -1,12 +1,12 @@
 class History
   def initialize(user_id, friend_id, access_token)
     @stream = Cache.get("#{user_id}_#{friend_id}") do
-      fetch(user_id, friend_id, access_token)
+      self.class.fetch(user_id, friend_id, access_token)
     end
   end
 
-  def fetch(user_id, friend_id, access_token)
-    query = "SELECT created_time, message, actor_id, source_id, comments, likes FROM stream WHERE source_id = #{user_id} AND actor_id = #{friend_id}"
+  def self.fetch(user_id, friend_id, access_token)
+    query = "SELECT created_time, message, actor_id, source_id, comments, likes, attachment FROM stream WHERE source_id = #{user_id} AND actor_id = #{friend_id}"
     response = access_token.get("https://api.facebook.com/method/fql.query", :query => query, :format => "json")
     results = JSON.parse(response)
     # if the api returns a hash, it's empty
